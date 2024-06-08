144bb3e935d3aaa3487621db940752c8af945ad2
Vaibhav Khinvasara <89930190+vkhinvasara@users.noreply.github.com>
2024-06-08 22:12:27 +0000

    shitty scroll animation added

--- src/routes/face.svelte
+++ src/routes/face.svelte
@@ -8,10 +8,18 @@
 	</div>
 	
 	<div class="profile-image">
-		<img src="/profile.jpg" alt="Profile Picture" />
+		<img src="/profile.jpg" alt="Vaibhav Khinvasara's Profile" />
 	</div>	
 </div>
-
+<div class="technologies-worked-with">
+	<div class="technologies">
+		<h2>Technologies worked with:</h2>
+	</div>
+	<div class="technologies-images">
+		<img src = "/django.svg" alt="Django" />
+		<img src = "/rust.svg" alt="Rust" />
+	</div>
+</div>
 <style>
 	.face-landing-page {
 		display: flex;
@@ -38,5 +46,37 @@
 
 	.profile-image img {
 		width: 100%;
+	}
+
+	@keyframes scroll{
+		0% {
+			transform: translateX(0);
+		}
+		100% {
+			transform: translateX(calc(-250px*7));
+		}
+	
+	}
+	.technologies-worked-with {
+		display: flex;
+		justify-content: space-around;
+		overflow-x: auto;
+		align-items: center;
+		width: 90%;
+		margin: 0.5rem auto;
+	}
+	.technologies {
+		width: 50%;
+	}
+	.technologies-images {
+		display: flex;
+		justify-content: flex-start;
+		margin: 1rem;
+		animation: scroll 40s linear infinite;
+	}
+	.technologies-images img{
+		width: 250px;
+		display: flex;
+		justify-content: space-around;
 	}
 </style>
